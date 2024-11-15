09915758d45d9c47d6f68ac96c3623775fdc38d4
LukasMueIIer <62990034+LukasMueIIer@users.noreply.github.com>
2024-11-15 22:55:06 +0100

    So working on the tutorial and functions

--- coaxial_jet.py
+++ coaxial_jet.py
@@ -1,20 +1,67 @@
 #provides a class to calculate coaxial jets with aerosandbox as its basis
 #therefore it must be used within an optimizer, especially to solve the implicit equations
+import aerosandbox.numpy as np
+import aerosandbox as asb
 
 class CoaxJet:
-    def __init__(self,Ui,Ua,Ti,Ta,d):
+    def __init__(self,Ui,Ua,Ti,Ta,d,opti: asb.Opti):
         #Ui is velocity of the Jet in [m/s]
         #Ua is velocity of the freestream [m/s]
         #Ti is temperature of the Jet [K]
         #Ta is temperature of the freestream [K]
         #d is diameter of the Jet
+        #opti is the optimizer class these calculations will be "appended to"
         self.Ui = Ui
         self.Ua = Ua
         self.Ti = Ti
         self.Ta = Ta
+        self.lam_0 = self.calculate_lam(Ui)
+        self.theta = self.calculate_theta()
+        self.opti = opti 
+        self.x_core = self.calculate_x_core()
     
-    def calcualte_lam(self,U):
+    def calcualte_lam(self,Uc):
         #calcualtes the lam value that corresponds to the given velocity
-        return self.Ua / (U - self.Ua)
+        return self.Ua / (Uc - self.Ua)
+
+    def calculate_Uc_from_lam(self,lam):
+        return self.Ua *((1/lam) + 1)
+    
+    def calculate_x_core(self):
+        #calculates the xv at which the core region (aka. constant Uc) ends
+        return self.calculate_xv(self.Ui)
 
     def calculate_theta(self): #calculates the theta for this jet
+         # Calculates theta for this jet
+        lam_0 = self.lam_0  # Calculate lambda_0 for the jet velocity Ui
+        theta_d_ratio = np.sqrt((np.pi / 4) * (self.Ti / self.Ta) * ((lam_0 + 1) / (lam_0 ** 2)))
+        theta = self.d * theta_d_ratio
+        return theta
+    
+    def calculate_xv(self, Uc):
+        # Calculate lambda for the given U
+        lam = self.calculate_lam(Uc)
+        # Calculate Lambda
+        Lambda = lam + 0.468
+        # Calculate xv/theta term
+        xv_theta_ratio = (
+            6.39 * Lambda ** (3 / 2) +
+            5.38 * Lambda ** (1 / 2) +
+            1.76 * Lambda ** (-1 / 2) -
+            8.3
+        )
+        # Calculate theta
+        theta = self.calculate_theta()
+        # Calculate xv
+        xv = theta * xv_theta_ratio
+        return xv
+    
+    def calculate_Uc_from_xv(self,x_v):
+        #implicitly calculates the responding centerline U_c value to a x_v value
+        l_U = self.opti.variable(self.Ua + 0.5 * (self.Ui - self.Ua))
+
+        #if we are within the core, setting l_U to Ui must return x_v
+        #if wer are smaller than x_core blend should be very negative and we should get the "first as output",
+        #where the equation is fullfiled if l_U equals Ui
+        self.opti.subject_to(x_v == np.blend(9999 * (self.x_core - x_v) ,self.calculate_xv(l_U))  ,self.x_v * l_U / self.Ui  )
+        return l_U
--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,14 @@
 #usage of the coaxial_jet.py class
+import aerosandbox as asb
+from coaxial_jet import *
 
+Ui = 10 #[m/s]
+Ua = 5 #[m/s]
+d = 0.1 #[m]
 
+opti = asb.Opti()
+
+jet = CoaxJet(Ui,Ua,1,1,d,opti) #creating the Jet class
+
+x_c = jet.calculate_x_core()
+print(x_c)
